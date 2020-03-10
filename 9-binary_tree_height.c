da9622bd08008236d0504e224950e41244cf2ee3
Alouie412 <alouie412@gmail.com>
2020-03-10 11:34:42 +0000

    Total rewrite: Helper functions not needed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,33 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * height_helper_left - Helper function. Recursively traverses
- * the left subtree and keeps count of recursions done
- * @tree: The current node
- * Return: The number of recursions done
- */
-size_t height_helper_left(const binary_tree_t *tree)
-{
-	if (tree != NULL)
-		return (1 + height_helper_left(tree->left));
-	else
-		return (0);
-}
-
-/**
- * height_helper_right - Helper function. Recursively traverses
- * the right subtree and keeps count of recursions done
- * @tree: The current node
- * Return: The number of recursions done
- */
-size_t height_helper_right(const binary_tree_t *tree)
-{
-	if (tree != NULL)
-		return (1 + height_helper_right(tree->right));
-	else
-		return (0);
-}
-/**
  * binary_tree_height - The driver function. Measures the height of
  * the binary tree at a particular node
  * @tree: The node to be measured
@@ -36,17 +9,15 @@ size_t height_helper_right(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height = 0, right_height = 0;
+	size_t count = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = height_helper_left(tree);
-	right_height = height_helper_right(tree);
+	if (tree->left != NULL)
+		count = 1 + binary_tree_height(tree->left);
+	if (tree->right != NULL)
+		count = 1 + binary_tree_height(tree->right);
 
-	/* Subtract 1. The leaf node is considered "ground level" */
-	if (left_height > right_height)
-		return (left_height - 1);
-	else
-		return (right_height - 1);
+	return (count);
 }
