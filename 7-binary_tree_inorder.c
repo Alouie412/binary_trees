ed8667fcca6a2099f1d338699b7b5256d62a825e
Alouie412 <alouie412@gmail.com>
2020-03-11 20:26:10 +0000

    Removed print_num, modified function calls

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,16 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * print_num - Prints a number
- *
- * @n: Number to be printed
- */
-void print_num(int n)
-{
-	printf("%d\n", n);
-}
-
-/**
  * binary_tree_inorder - Function that traverses and prints
  * the binary tree values using inorder method
  * @tree: The current node
@@ -22,7 +12,7 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	binary_tree_inorder(tree->left, &print_num);
-	print_num(tree->n);
-	binary_tree_inorder(tree->right, &print_num);
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,16 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * print_num - Prints a number
- *
- * @n: Number to be printed
- */
-void print_num(int n)
-{
-	printf("%d\n", n);
-}
-
-/**
  * binary_tree_postorder - Function that traverses and prints
  * the binary tree values using postorder method
  * @tree: The current node
@@ -22,7 +12,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	binary_tree_postorder(tree->left, &print_num);
-	binary_tree_postorder(tree->right, &print_num);
-	print_num(tree->n);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
