968f9d1a874e097f6ddb0b19dc90872be3ec48f3
Alouie412 <alouie412@gmail.com>
2020-03-11 22:30:41 +0000

    Total rewrite: added helper function binary_tree_height2

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,25 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height2 - Helper function. Measures the height of
+ * the binary tree at a particular node
+ * This is actually the same as 9-binary_tree_height.c
+ * @tree: The node to be measured
+ * Return: 0 if tree is NULL, otherwise the distance between node
+ * and "ground level"
+ */
+size_t binary_tree_height2(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left != NULL)
+		return (1 + binary_tree_height2(tree->left));
+	else
+		return (1 + binary_tree_height2(tree->right));
+}
+
+/**
  * binary_tree_balance - Function that determines the balance
  * factor of a binary tree. In other words, checks the height
  * of the left and right subtree from the node and return the
@@ -16,10 +35,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
-		left_count = 1 + binary_tree_balance(tree->left);
-	if (tree->right != NULL)
-		right_count = 1 + binary_tree_balance(tree->right);
+	left_count = binary_tree_height2(tree->left);
+	right_count = binary_tree_height2(tree->right);
 
 	return (left_count - right_count);
 }
