d2b3665b23ba586762348a018e11fd2cfcfc7eb3
Alouie412 <alouie412@gmail.com>
2020-03-11 20:21:12 +0000

    Removed print_num, changed function pointer call

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,16 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * print_num - Prints a number
- *
- * @n: Number to be printed
- */
-void print_num(int n)
-{
-	printf("%d\n", n);
-}
-
-/**
  * binary_tree_preorder - Function that traverses and prints
  * the binary tree values using preorder method
  * @tree: The current node
@@ -22,7 +12,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || func == NULL)
 		return;
 
-	print_num(tree->n);
+	func(tree->n);
 	binary_tree_preorder(tree->left, func);
 	binary_tree_preorder(tree->right, func);
 }
