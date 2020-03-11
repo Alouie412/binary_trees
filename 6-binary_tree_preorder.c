bac89817a23a9bd6b8981974547e1b876db561ae
Alouie412 <alouie412@gmail.com>
2020-03-11 19:21:34 +0000

    Added missing function print_num

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,6 +1,16 @@
 #include "binary_trees.h"
 
 /**
+ * print_num - Prints a number
+ *
+ * @n: Number to be printed
+ */
+void print_num(int n)
+{
+	printf("%d\n", n);
+}
+
+/**
  * binary_tree_preorder - Function that traverses and prints
  * the binary tree values using preorder method
  * @tree: The current node
