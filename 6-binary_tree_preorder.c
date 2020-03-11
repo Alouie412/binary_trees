844e11b716ff5ca8e7639bf7e05114559523d337
Alouie412 <alouie412@gmail.com>
2020-03-11 19:36:03 +0000

    Removed print_num function, fixed argument pass in

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,16 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * print_num - Prints a number
- *
- * @n: Number to be printed
- */
-void print_num(int n)
-{
-	printf("%d\n", n);
-}
-
-/**
  * binary_tree_preorder - Function that traverses and prints
  * the binary tree values using preorder method
  * @tree: The current node
@@ -23,6 +13,6 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	print_num(tree->n);
-	binary_tree_preorder(tree->left, tree->left->n);
-	binary_tree_preorder(tree->right, tree->left->n);
+	binary_tree_preorder(tree->left, print_num);
+	binary_tree_preorder(tree->right, print_num);
 }
