915c5afda6a323789d16b072dcf3fdce706b89cf
Alouie412 <alouie412@gmail.com>
2020-03-11 20:28:01 +0000

    Added description explaining usage of function ptr

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -6,6 +6,12 @@
  * @tree: The current node
  * @func: The passed in function
  * Return: Nothing
+ * Description: For this particular task, we needed to print out
+ * the value at the node. The function print_num given to us would
+ * have sufficed, but is actually incorrect because we are not supposed
+ * to use print_num. The function pointer is used so that we could
+ * use this same code for other applications, such as duplicating the
+ * node's value
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
